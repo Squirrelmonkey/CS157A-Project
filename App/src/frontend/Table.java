576c26e3a331d3e04cce64bfa4d9b8da6263ac6a
Squirrelmonkey <mtlegler@gmail.com>
2022-08-02 13:18:29 -0700

    t

--- App/src/frontend/Table.java
+++ App/src/frontend/Table.java
@@ -1,8 +1,11 @@
 package frontend;
 
+
 import javax.swing.*;
 
 import java.awt.*;
+import java.io.*;
+
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -14,41 +17,33 @@ public class Table extends JPanel {
 /*
     public SimpleTableDemo() {
         super(new GridLayout(1,0));
-
         String[] columnNames = {"First Name",
                 "Last Name",
                 "Sport",
                 "# of Years",
                 "Vegetarian"};
-
         Object[][] data = {
                 {"Kathy", "Smith",
                         "Snowboarding", 5, Boolean.FALSE},
                 {"John", "Doe",
                         "Rowing", 3, Boolean.TRUE},
         };
-
         final JTable table = new JTable(data, columnNames);
         table.setPreferredScrollableViewportSize(new Dimension(500, 70));
         table.setFillsViewportHeight(true);
-
         //Create the scroll pane and add the table to it.
         JScrollPane scrollPane = new JScrollPane(table);
-
         //Add the scroll pane to this panel.
         add(scrollPane);
     }
-
     static void createAndShowGUI() {
         //Create and set up the window.
         JFrame frame = new JFrame("SimpleTableDemo");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
         //Create and set up the content pane.
         SimpleTableDemo newContentPane = new SimpleTableDemo();
         newContentPane.setOpaque(true); //content panes must be opaque
         frame.setContentPane(newContentPane);
-
         //Display the window.
         frame.pack();
         frame.setVisible(true);
@@ -64,29 +59,31 @@ public class Table extends JPanel {
                     ex.printStackTrace();
                 }
 
+                /* 
                 String[] columnNames = {"First Name",
                         "Last Name",
                         "Sport",
                         "# of Years",
                         "Vegetarian","Vegetarian","Vegetarian","Vegetarian"};
-
+                
                 Object[][] data = {
-                        {"Kathy", "Smith",
-                                "Snowboarding", 5, "Boolean.FALSE"},
-                        {"John", "Doe",
-                                "Rowing", 3, "Boolean.TRUE"},
-                        {"baa", "aoe",
-                                "Rowing", 6, "Boolean.TRUE"},
-                        {"aaa", "Doe",
-                                "Rowing", 1, "Boolean.TRUE"},
+                        {"Kathy", "Smith"},
+                        {"John", "Doe"},
+                        {"baa", "aoe"},
+                        {"aaa", "Doe"},
                 };
+                */
+
+                String[] columnNames = getCollumnNames();
+                Object[][] data = getData();
+
 
                 DefaultTableModel model = new DefaultTableModel(data, columnNames);
                 /*model.addRow(new Object[]{"Prankster", "USA", "Microsoft Pvt Ltd", "Party'14", 900000});
                 model.addRow(new Object[]{"Prankster", "USA", "crosoft Pvt Ltd", "Party'14", 900000});
                 model.addRow(new Object[]{"rankster", "UA", "Microoft Pvt Ltd", "Party'14", 900000});
                 model.addRow(new Object[]{"Prankster", "USA", "Microsoft Pvt Ltd", "Party'14", 900000});
-*/
+                */
                 JTable table = new JTable(model);
                 TableRowSorter<TableModel> sorter = new TableRowSorter<TableModel>(table.getModel());
                 table.setRowSorter(sorter);
@@ -94,7 +91,7 @@ public class Table extends JPanel {
                 List<RowSorter.SortKey> sortKeys = new ArrayList<>(25);
                 sortKeys.add(new RowSorter.SortKey(0, SortOrder.ASCENDING));
                 sortKeys.add(new RowSorter.SortKey(1, SortOrder.ASCENDING));
-                sorter.setSortKeys(sortKeys);
+                //sorter.setSortKeys(sortKeys);
 
                 JFrame frame = new JFrame(title);
 
@@ -103,7 +100,7 @@ public class Table extends JPanel {
                     frame.dispose();
                     try {
                         Application.menu();
-                    } catch (IOException ex) {
+                    } catch (Exception ex) {
                         ex.printStackTrace();
                     }
                 });
@@ -122,5 +119,58 @@ public class Table extends JPanel {
                 frame.setVisible(true);
             }
         });
+    }
+
+    public String[] getCollumnNames(){
+        
+        try{
+            FileInputStream fstream = new FileInputStream("results.txt");
+            DataInputStream in = new DataInputStream(fstream);
+            BufferedReader br = new BufferedReader(new InputStreamReader(in));
+            String strLine = br.readLine();
+            String[] collumns  = strLine.split("\\t");
+            return collumns;
+        } catch (Exception e){
+            System.err.println("Error: " + e.getMessage());
+            String[] empty = new String[0];
+            return empty;
+        }        
+    }
+
+    public Object[][] getData(){
+
+        int lines = 0;
+        try{
+            BufferedReader reader = new BufferedReader(new FileReader("results.txt"));
+            while (reader.readLine() != null) lines++;
+            lines--;
+            reader.close();
+        } catch (Exception e){
+            System.err.println("Error: " + e.getMessage());
+            
+        }
+
+        try{
+            FileInputStream fstream = new FileInputStream("results.txt");
+            DataInputStream in = new DataInputStream(fstream);
+            BufferedReader br = new BufferedReader(new InputStreamReader(in));
+            String strLine = br.readLine();
+            String[] rowData = strLine.split("\\t");
+            Object[][] data = new Object[lines][rowData.length];
+            int i = -1;
+            while ((strLine = br.readLine()) != null)   {
+                i++;
+                rowData = strLine.split("\\t");
+                for(int j = 0; j < rowData.length; j++){
+                    data[i][j] = rowData[j];
+                } 
+             }
+             return data;
+        } catch (Exception e){
+            System.err.println("Error: " + e.getMessage());
+            Object[][] empty = new Object[0][0];
+            return empty;
+        }
+
     }
 }
